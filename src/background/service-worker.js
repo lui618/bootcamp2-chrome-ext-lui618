5d542d2e051b0f7fef6405c99a95e277681bf39a
Luis Filipe <luisfilipecab@gmail.com>
2025-09-13 20:29:37 -0300

    feito index.html em docs

--- src/background/service-worker.js
+++ src/background/service-worker.js
@@ -1,3 +1,10 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Link Highlighter instalado.');
+  console.log('Bootcamp Helper instalado.');
+  chrome.storage.local.set({ installs: (Date.now()) });
+});
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.type === 'PING') {
+    sendResponse({ ok: true, time: new Date().toISOString() });
+  }
 });
